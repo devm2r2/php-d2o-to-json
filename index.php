aad0023cc9ae1dfd4b333e63cdc785e9516220a1
devm2r2 <31595078+devm2r2@users.noreply.github.com>
2017-09-03 18:12:45 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//Init Debug stuff
+ini_set('xdebug.var_display_max_depth', 5);
+ini_set('xdebug.var_display_max_children', 256);
+ini_set('xdebug.var_display_max_data', 1024);
+
+
+
+require 'php/D2OReader.class.php';
+
+
+//Convert d2o to json
+$d2o = new D2OReader('d2o/AbuseReasons.d2o');
+$json = $d2o->json();
+var_dump($json);
+
+
