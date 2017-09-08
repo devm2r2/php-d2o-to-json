7dcf89decc9a10e9e3635278b7637764c7cd4798
devm2r2 <31595078+devm2r2@users.noreply.github.com>
2017-09-08 20:45:37 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,34 @@
-<?php
+<?php initDebug();
 
-//Init Debug stuff
-ini_set('xdebug.var_display_max_depth', 16);
-ini_set('xdebug.var_display_max_children', 256);
-ini_set('xdebug.var_display_max_data', 1024);
-
-
-
+// Import
 require 'php/D2OReader.class.php';
+require 'php/D2OWriter.class.php';
+
+// Convert
+$json = '';
+d2o_to_json($json);
+json_to_d2o($json);
 
 
-//Convert d2o to json
-$d2o = new D2OReader('d2o/Jobs.d2o');
-$json = $d2o->json();
-
-//Output
-var_dump($json);
 
 
+
+function initDebug()
+{
+	//Init Debug stuff
+	ini_set('xdebug.var_display_max_depth', 16);
+	ini_set('xdebug.var_display_max_children', 256);
+	ini_set('xdebug.var_display_max_data', 1024);
+}
+function d2o_to_json(&$json)
+{
+	$D2o = new D2OReader('d2o/Jobs.d2o');
+	$json = $D2o->json();
+	var_dump($json);
+}
+function json_to_d2o(&$json)
+{
+	$D2o = new D2OWriter($json);
+	$bin = $D2o->d2o();
+	var_dump($bin);
+}
